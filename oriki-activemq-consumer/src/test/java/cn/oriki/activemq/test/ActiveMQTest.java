f8343e6f68850fa21d9f218abc4e0242a3567a47
oriki <n0rk1n@live.cn>
2018-01-15 16:14:07 +0800

    activemq消费者的使用

---
+++ oriki-activemq-consumer/src/test/java/cn/oriki/activemq/test/ActiveMQTest.java
@@ -0,0 +1,53 @@
+package cn.oriki.activemq.test;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Test;
+
+import javax.jms.*;
+
+public class ActiveMQTest {
+
+    @Test
+    public void test() throws Exception {
+        //第一步：建立连接
+        //连接工厂：所有参数默认
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory
+                ("admin", "admin", "tcp://172.16.191.201:61616");
+
+        //获取连接
+        Connection connection = connectionFactory.createConnection();
+
+        //开启连接
+        connection.start();
+
+        //第二步：获取session操作对象，进行操作
+        //参数1：是否开启手动事务，true:事务需要手动提交，mq才会将消息持久化保存，和数据库类似
+        //参数2：一旦手动事务，第二个参数无效，但一般写自动。自动处理消息机制
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        //1)先制造一个消息队列（容器）,这里是一个q的消息-将来就从该队列中读消息
+        Queue queue = session.createQueue("myQueue");
+
+        //2)根据消息，创建一个消费者，该消费者只能消费指定队列类型的队列的消息
+        MessageConsumer consumer = session.createConsumer(queue);
+
+        //3)获取消息的内容（消息对象）
+        while (true) {
+//			Message message = consumer.receive();//获取不到消息立即返回空
+            Message message = consumer.receive(5000);//如果多少毫秒获取不到消息，则直接返回空消息。
+            if (message != null) {
+                String str = ((TextMessage) message).getText();
+                System.out.println(str);
+            } else {
+                System.out.println("消息处理完毕，退出消费。。。");
+                break;
+            }
+        }
+
+        //关闭连接
+        consumer.close();
+        session.close();
+        connection.stop();
+        connection.close();
+    }
+}
