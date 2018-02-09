8016dbcc5a2c2c7ec16c521dc03a6f50ee61a2da
oriki <n0rk1n@live.cn>
2018-02-09 10:17:02 +0800

    使用SpringCloud实现微服务（准备工作）——解决硬编码问题

--- oriki-springcloud-order/src/main/java/cn/oriki/springcloud/service/ItemService.java
+++ oriki-springcloud-order/src/main/java/cn/oriki/springcloud/service/ItemService.java
@@ -2,6 +2,7 @@ package cn.oriki.springcloud.service;
 
 import cn.oriki.springcloud.domain.Item;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,8 +12,11 @@ public class ItemService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${oriki.item.url}")
+    private String itemProjectUrl;
+
     public Item queryItemById(Long id) {
-        return this.restTemplate.getForObject("http://127.0.0.1:8081/item/"
+        return this.restTemplate.getForObject(itemProjectUrl + "/item/"
                 + id, Item.class);
     }
 
