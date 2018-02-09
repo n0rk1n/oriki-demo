a08929c883983c4f4bda6b45019fffadcd119493
oriki <n0rk1n@live.cn>
2018-02-09 10:06:34 +0800

    使用SpringCloud实现微服务（准备工作）——添加okhttp的支持

--- oriki-springcloud-order/src/main/java/cn/oriki/springcloud/OrderApplication.java
+++ oriki-springcloud-order/src/main/java/cn/oriki/springcloud/OrderApplication.java
@@ -3,14 +3,20 @@ package cn.oriki.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrderApplication {
 
     @Bean
+    public OkHttp3ClientHttpRequestFactory okHttp3ClientHttpRequestFactory() {
+        return new OkHttp3ClientHttpRequestFactory();
+    }
+
+    @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(okHttp3ClientHttpRequestFactory());
     }
 
     public static void main(String[] args) {
