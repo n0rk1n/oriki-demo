04779f75c576a5e8fcc68ae586975744de1ab0e0
oriki <n0rk1n@live.cn>
2018-02-12 11:52:54 +0800

    Hystrix实现容错处理

--- oriki-springcloud-order/src/main/java/cn/oriki/springcloud/OrderApplication.java
+++ oriki-springcloud-order/src/main/java/cn/oriki/springcloud/OrderApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class OrderApplication {
 
     @Bean
--- oriki-springcloud-order/src/main/java/cn/oriki/springcloud/service/ItemService.java
+++ oriki-springcloud-order/src/main/java/cn/oriki/springcloud/service/ItemService.java
@@ -1,6 +1,7 @@
 package cn.oriki.springcloud.service;
 
 import cn.oriki.springcloud.domain.Item;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
@@ -15,10 +16,15 @@ public class ItemService {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @HystrixCommand(fallbackMethod = "queryItemByIdFallBack")// 调用失败后执行的请求
     public Item queryItemById(Long id) {
         String serviceId = "oriki-springcloud-item";
         String url = "http://" + serviceId + "/item/" + id;
         return this.restTemplate.getForObject(url, Item.class);
+    }
+
+    public Item queryItemByIdFallBack(Long id) {
+        return new Item(id, "查询商品信息出错!", null, null, null);
     }
 
 }
