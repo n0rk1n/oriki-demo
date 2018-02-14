3480649d0802fda31155daf94e1940a6d3f90a27
oriki <n0rk1n@live.cn>
2018-02-14 18:33:54 +0800

    SpringCloudConfig优化

--- oriki-springcloud-config-client/src/main/java/cn/oriki/springcloud/web/controller/ConfigController.java
+++ oriki-springcloud-config-client/src/main/java/cn/oriki/springcloud/web/controller/ConfigController.java
@@ -1,9 +1,9 @@
 package cn.oriki.springcloud.web.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.cloud.context.config.annotation.RefreshScope;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
