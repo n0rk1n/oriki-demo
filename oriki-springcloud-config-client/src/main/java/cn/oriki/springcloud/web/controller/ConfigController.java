31638c6577bd9adc1ca431dc91b7760be24421f2
oriki <n0rk1n@live.cn>
2018-02-14 17:45:22 +0800

    SpringCloudConfig

---
+++ oriki-springcloud-config-client/src/main/java/cn/oriki/springcloud/web/controller/ConfigController.java
@@ -0,0 +1,19 @@
+package cn.oriki.springcloud.web.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigController {
+
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @GetMapping("/config")
+    public String config(){
+        return jdbcUrl;
+    }
+}
